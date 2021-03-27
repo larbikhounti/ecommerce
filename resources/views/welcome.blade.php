240029e00adcd63efea3d84d5e5d8785b689f2f3
larbikhounti <larbikhounti@gmail.com>
2021-03-27 20:26:28 +0100

    project structure

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('customcss')
+<link href="{{ asset('css/mywelcome.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+<div class="container text-center">
+    <h1 class="fs-5">K-Ecommerce</h1> 
+</div>
+
+    
+@endsection
